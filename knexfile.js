f10374b71a31b812c6b284124e25bbad1a1ec33d
Ashley Dunham <xenocide21@gmail.com>
2020-07-22 17:22:23 -0700

    updated production to development

--- knexfile.js
+++ knexfile.js
@@ -55,18 +55,21 @@ module.exports = {
 	},
 
 	production: {
-		client: "postgresql",
+		client: "sqlite3",
+		useNullAsDefault: true,
 		connection: {
-			database: "my_db",
-			user: "username",
-			password: "password",
+			filename: "./database/howtodos.db3",
 		},
 		pool: {
-			min: 2,
-			max: 10,
+			afterCreate: (conn, done) => {
+				conn.run("PRAGMA foreign_keys = ON", done);
+			},
 		},
 		migrations: {
-			tableName: "knex_migrations",
+			directory: "./database/migrations",
+		},
+		seeds: {
+			directory: "./database/seeds",
 		},
 	},
 };
